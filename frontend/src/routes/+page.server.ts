94c4659e4138a744e54a382fe4acaec75c5f2d17
Voodoochilee16 <paulo.carvalhoferreira@protonmail.com>
2025-05-04 18:13:19 +0100

    updated npm command, npm run start now will make the whole stack run in dev mode.

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-const API = 'http://localhost:8080';
+const API = 'http://app:8080'; // Changed from 'http://localhost:8080'
 import { error } from '@sveltejs/kit';
 
 export async function load({ locals }) {

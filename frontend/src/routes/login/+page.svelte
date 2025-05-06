dd5032895e411170ad8ea7776de42c860e27c014
strangecousinwst <139879737+strangecousinwst@users.noreply.github.com>
2025-05-06 14:01:15 +0100

    login and register layout?

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -2,52 +2,47 @@
 	const { data } = $props();
 
 	console.log('Page Data Login', data);
-
-	// user info
-	// let username = '';
-	// let email = '';
-	// let password = '';
-	// let bio = '';
-	// idk, gpt
-	// let error: string | null = null;
-	// let loading = false;
 </script>
 
-<div class="md:hidden"></div>
-<div
-	class="relative container hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
->
-	<button class="absolute top-4 right-4 md:top-8 md:right-8"> Login </button>
-	<div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
-		<div class="absolute inset-0 bg-cover"></div>
-		<div class="relative z-20 flex items-center text-lg font-medium">
-			<!-- <Command class="mr-2 h-6 w-6" /> -->
-			Go Workout
-		</div>
-		<div class="relative z-20 mt-auto">
-			<blockquote class="space-y-2">
-				<p class="text-lg">&ldquo;Postgres is better than Tailwind&rdquo;</p>
-				<footer class="text-sm">Bill Gates</footer>
-			</blockquote>
-		</div>
-	</div>
-	<div class="lg:p-8">
-		<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+<div class="main">
+	<div class="absolute top-4 left-4">Go Workout</div>
+	<button class="absolute top-4 right-4">Login</button>
+	<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+		<div class="cabecalho">
 			<div class="flex flex-col space-y-2 text-center">
-				<h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
-				<p class="text-muted-foreground text-sm">Enter your email below to create your account</p>
+				<div class="flex flex-col space-y-2 text-center">
+					<h1 class="text-2xl font-semibold tracking-tight">Login to your account</h1>
+					<p class="text-muted-foreground text-sm">Enter your email below to login your account</p>
+				</div>
+				<form action="user_login" method="post">
+					<div class="flex flex-col text-center">
+						<label for="username"><b>Username</b></label>
+						<input type="text" placeholder="Enter Username" name="username" required />
+						<label for="password"><b>Password</b></label>
+						<input type="password" placeholder="Enter password" name="password" required />
+						<button type="submit">Login</button>
+						<label>
+							<input type="checkbox" checked="checked" name="remember" /> Remember me
+						</label>
+					</div>
+					<div class="container">
+						<button type="button" class="cancelbtn">Cancel</button>
+						<span class="psw">Forgot <a href="placeholder">password?</a></span>
+					</div>
+				</form>
+				<p class="text-muted-foreground px-8 text-center text-sm">
+					Dont have account yet?
+					<a href="/register" class="hover:text-primary underline underline-offset-4">
+						Click here to register
+					</a>
+				</p>
 			</div>
-			<p class="text-muted-foreground px-8 text-center text-sm">
-				By clicking continue, you agree to our
-				<a href="/terms" class="hover:text-primary underline underline-offset-4">
-					Terms of Service
-				</a>
-				and
-				<a href="/privacy" class="hover:text-primary underline underline-offset-4">
-					Privacy Policy
-				</a>
-				.
-			</p>
+			<div class="absolute bottom-4 left-4">
+				<blockquote class="space-y-2">
+					<p class="text-lg">&ldquo;Postgres is better than Tailwind&rdquo;</p>
+					<footer class="text-sm">Bill Gates</footer>
+				</blockquote>
+			</div>
 		</div>
 	</div>
 </div>
--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,1 +1,51 @@
-<h1 class="text-2xl text-orange-300">Register page</h1>
+<script lang="ts">
+	const { data } = $props();
+
+	console.log('Page Data Register', data);
+</script>
+
+<div class="main">
+	<div class="absolute top-4 left-4">Go Workout</div>
+	<button class="absolute top-4 right-4">Register</button>
+	<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+		<div class="cabecalho">
+			<div class="flex flex-col space-y-2 text-center">
+				<h1 class="text-2xl font-semibold tracking-tight">Create account</h1>
+				<p class="text-muted-foreground text-sm">Enter the details below to create a account</p>
+			</div>
+			<form action="user_register" method="post">
+				<div class="flex flex-col text-center">
+					<label for="username"><b>Username</b></label>
+					<input type="text" placeholder="Enter Username" name="username" required />
+					<label for="email"><b>Email</b></label>
+					<input type="text" placeholder="Enter Email" name="email" required />
+					<label for="password"><b>Password</b></label>
+					<input type="password" placeholder="Enter Password" name="password" required />
+					<br />
+					<button type="submit">Register</button>
+					<br />
+					<label>
+						<input type="checkbox" checked="checked" name="remember" /> Remember me
+					</label>
+				</div>
+			</form>
+			<p class="text-muted-foreground px-8 text-center text-sm">
+				By clicking continue, you agree to our
+				<a href="/terms" class="hover:text-primary underline underline-offset-4">
+					Terms of Service
+				</a>
+				and
+				<a href="/privacy" class="hover:text-primary underline underline-offset-4">
+					Privacy Policy
+				</a>
+				.
+			</p>
+		</div>
+	</div>
+	<div class="absolute bottom-4 left-4">
+		<blockquote class="quote">
+			<p class="text">&ldquo; You miss 100% of the shots you don't take. -Wayne Gretzky.&rdquo;</p>
+			<footer class="author">Michael Scott</footer>
+		</blockquote>
+	</div>
+</div>
